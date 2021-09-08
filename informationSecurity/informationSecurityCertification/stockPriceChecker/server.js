925171bc7f225229ff188c4bc4d425b836b99a77
gary003 <gary.johnson003@gmail.com>
2021-09-08 16:32:55 +0200

    WIP: Stock Price Checker

---
+++ informationSecurity/informationSecurityCertification/stockPriceChecker/server.js
@@ -0,0 +1,75 @@
+"use strict"
+require("dotenv").config()
+
+const express = require("express")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+
+const apiRoutes = require("./routes/api.js")
+const fccTestingRoutes = require("./routes/fcctesting.js")
+const runner = require("./test-runner")
+const helmet = require("helmet")
+
+const app = express()
+
+app.use("/public", express.static(process.cwd() + "/public"))
+
+app.use(cors({ origin: "*" })) //For FCC testing purposes only
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      // enable and configure
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'"],
+        styleSrc: ["'self'"]
+      }
+    }
+  })
+)
+
+//Index page (static HTML)
+app.route("/").get((req, res) => {
+  return res.status(200).sendFile(process.cwd() + "/views/index.html")
+})
+
+app.route("/api/stock-prices").get((req, res) => {
+  const dataResult = {
+    stockData: req.query.stock
+  }
+
+  return res.status(200).send(dataResult)
+})
+
+//For FCC testing purposes
+fccTestingRoutes(app)
+
+//Routing for API
+apiRoutes(app)
+
+//404 Not Found Middleware
+app.use((req, res, next) => {
+  return res.status(404).type("text").send("Not Found")
+})
+
+//Start our server and tests!
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log(`Your app is listening on port ${listener.address().port}`)
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...")
+    setTimeout(function () {
+      try {
+        runner.run()
+      } catch (e) {
+        console.log("Tests are not valid:")
+        console.error(e)
+      }
+    }, 3500)
+  }
+})
+
+module.exports = app //for testing
