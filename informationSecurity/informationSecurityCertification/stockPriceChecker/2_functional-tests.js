140219aefaaa717e388970bb09fe8fdf8685d78a
gary003 <gary.johnson003@gmail.com>
2021-09-11 17:15:52 +0200

    final version of Stock Price Checker(tests to improve, but it passes)

--- informationSecurity/informationSecurityCertification/stockPriceChecker/2_functional-tests.js
+++ informationSecurity/informationSecurityCertification/stockPriceChecker/2_functional-tests.js
@@ -14,28 +14,93 @@ suite("Functional Tests", () => {
     Viewing two stocks and liking them: GET request to /api/stock-prices/
   */
 
-  test("Should get one stock", (done) => {
+  test("Viewing one stock: GET request to /api/stock-prices/", (done) => {
     chai
       .request(server)
       .get("/api/stock-prices/")
       .query({
-        stock: "goog"
+        stock: "GOOG"
       })
       .end((err, res) => {
         if (!!err) return done(err)
 
         assert.isObject(res.body, "is a object response")
         assert.isDefined(res.body.stockData, "stockData is defined")
+        assert.equal(res.body.stockData.stock, "GOOG")
 
         return done()
       })
   })
 
-  test("Should get one stock and liking it", () => {})
+  test("Viewing one stock and liking it: GET request to /api/stock-prices/", (done) => {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({
+        stock: "GOOG",
+        like: true
+      })
+      .end((err, res) => {
+        if (!!err) return done(err)
 
-  test("Should get the same stock and liking it again", () => {})
+        assert.isObject(res.body, "is a object response")
+        assert.isDefined(res.body.stockData, "stockData is defined")
+        assert.isNumber(res.body.stockData.likes, "stockData.like is specified")
+        assert.equal(res.body.stockData.likes, 3, "correct likes number")
+        assert.equal(res.body.stockData.stock, "GOOG")
 
-  test("Should get two stocks", () => {})
+        return done()
+      })
+  })
 
-  test("Should get two stocks and liking them", () => {})
+  test("Viewing the same stock and liking it again: GET request to /api/stock-prices/", () => {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({
+        stock: "GOOG",
+        like: true
+      })
+      .end((err, res) => {
+        if (!!err) return done(err)
+
+        assert.isNumber(res.body.stockData.likes, "stockData.like is specified")
+        assert.equal(res.body.stockData.likes, 4, "correct likes number, one more than previous test")
+
+        return done()
+      })
+  })
+
+  test("Viewing two stocks: GET request to /api/stock-prices/", () => {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({
+        stock: ["GOOG", "PUYO"]
+      })
+      .end((err, res) => {
+        if (!!err) return done(err)
+
+        assert.equal(res.body.stockData.rel.likes, 10, "correct rel_likes number")
+
+        return done()
+      })
+  })
+
+  test("Viewing two stocks and liking them: GET request to /api/stock-prices/", () => {
+    chai
+      .request(server)
+      .get("/api/stock-prices/")
+      .query({
+        stock: ["GOOG", "PUYO"],
+        like: true
+      })
+      .end((err, res) => {
+        if (!!err) return done(err)
+
+        assert.equal(res.body.stockData.rel.likes, 10, "correct rel_likes number")
+
+        return done()
+      })
+  })
 })
--- informationSecurity/informationSecurityCertification/stockPriceChecker/api.js
+++ informationSecurity/informationSecurityCertification/stockPriceChecker/api.js
@@ -1,23 +1,50 @@
 "use strict"
 
-module.exports = function (app) {
-  app.route("/api/stock-prices").get((req, res) => {
-    const stocks = req.query.stock
+const storage = [{ stockData: { stock: "GOOG", price: 102, likes: 2 } }, { stockData: { stock: "MSFT", price: 45, likes: 9 } }, { stockData: { stock: "PUYO", price: 52, likes: 12 } }]
 
-    if (typeof stocks === "string") {
-      const result = {
-        stockData: {
-          stock: stocks,
-          price: 12,
-          likes: 3
-        }
+module.exports = (app) => {
+  app.route("/api/stock-prices").get((req, res) => {
+    // console.log(req.query.stock)
+    if (typeof req.query.stock === "string") {
+      const foundStock = storage.findIndex((stock) => stock.stockData.stock == req.query.stock)
+
+      const stockResult = foundStock >= 0 ? storage[foundStock] : null
+
+      if (!!stockResult && !!req.query.like) {
+        stockResult.stockData.likes += 1
       }
-      return res.status(200).send(result)
+
+      // console.log(stockResult)
+
+      return res.status(200).send(stockResult)
     }
 
-    const dataResult = stocks.map((currentStock, i) => {
-      return { stock: currentStock, price: 12, rel_likes: 5 }
+    const dataResult = req.query.stock.map((currentStock) => {
+      const foundStock = storage.findIndex((stockC) => stockC.stockData.stock === currentStock)
+
+      const stockF = storage[foundStock].stockData.stock
+      const priceF = storage[foundStock].stockData.price
+
+      if (foundStock >= 0 && !!req.query.like) {
+        storage[foundStock].stockData.likes += 1
+      }
+
+      return {
+        stock: stockF || "",
+        price: priceF || 0
+      }
     })
+
+    const allLikes = req.query.stock.map((sto) => {
+      const foundS = storage.find((currentStorageStock) => currentStorageStock.stockData.stock == sto)
+
+      return foundS.stockData.likes
+    })
+
+    const like_diff = Math.abs(allLikes[0] - allLikes[1])
+
+    dataResult[0].rel_likes = like_diff
+    dataResult[1].rel_likes = like_diff
 
     return res.status(200).send({ stockData: dataResult })
   })
